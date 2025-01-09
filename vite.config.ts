30a61183682d406ecdc1bc8b46621a10b57c0413
okba14 <117593772+okba14@users.noreply.github.com>
2025-01-09 11:00:12 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Tourism-City-App-E.lkantara/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
